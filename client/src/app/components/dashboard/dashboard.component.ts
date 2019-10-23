c329e075a109f71f8ad86a1aa87bf26ca258f367
Brian Froschauer <brian.froschauer@ing.austral.edu.ar>
2019-10-23 18:10:05 -0300

    (feature): add proxy dashboard

--- client/src/app/components/dashboard/dashboard.component.ts
+++ client/src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {Statistic} from '../../model/statistic.interface';
+import {Component, OnInit} from '@angular/core';
 import {StatisticService} from '../../services/statistics/statistic.service';
+import {Statistic} from '../../model/statistic.interface';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,18 +9,71 @@ import {StatisticService} from '../../services/statistics/statistic.service';
 })
 export class DashboardComponent implements OnInit {
 
-  statistics: Statistic[];
+  public statistics: Statistic[] = [];
+  public data: any[] = [];
 
-  constructor(private statisticService: StatisticService) { }
+  constructor(private statisticService: StatisticService) {
+  }
 
   ngOnInit() {
-    this.statistics = [];
     this.getStatistics();
+  }
+
+  setChartData() {
+    this.getTotal();
+    this.getSuccess();
+    this.getError();
+    this.getRateLimit();
+  }
+
+  getTotal() {
+    const total = { 'name': 'Total', 'series': []};
+    this.statistics.forEach(statistic => {
+      total.series.push({
+        name: new Date(statistic.date),
+        value: statistic.total
+      });
+    });
+    this.data.push(total);
+  }
+
+  getSuccess() {
+    const success = { 'name': 'Success', 'series': []};
+    this.statistics.forEach(statistic => {
+      success.series.push({
+        name: new Date(statistic.date),
+        value: statistic.success
+      });
+    });
+    this.data.push(success);
+  }
+
+  getError() {
+    const error = { 'name': 'Error', 'series': []};
+    this.statistics.forEach(statistic => {
+      error.series.push({
+        name: new Date(statistic.date),
+        value: statistic.error
+      });
+    });
+    this.data.push(error);
+  }
+
+  getRateLimit() {
+    const rateLimit = { 'name': 'Rate Limit', 'series': []};
+    this.statistics.forEach(statistic => {
+      rateLimit.series.push({
+        name: new Date(statistic.date),
+        value: statistic.rateLimit
+      });
+    });
+    this.data.push(rateLimit);
   }
 
   getStatistics() {
     this.statisticService.findAll().subscribe(statistics => {
       this.statistics = statistics;
+      this.setChartData();
     });
   }
 }
